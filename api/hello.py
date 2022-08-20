349b1e456d44843adedd697fb787e309c0d59521
Fancc666 <60925834+Fancc666@users.noreply.github.com>
2022-08-20 13:18:32 +0800

    Update hello.py

--- api/hello.py
+++ api/hello.py
@@ -1,10 +1,10 @@
 from http.server import BaseHTTPRequestHandler
-
+from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'texxt/plain')
-        self.end_headers()
-        self.wfile.write("hello world! vercel".encode())
-        return
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    return
