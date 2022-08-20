7e96acd45ea45b079a44c8df17131778e8e8cd97
Fancc666 <60925834+Fancc666@users.noreply.github.com>
2022-08-20 13:31:18 +0800

    Update hello.py

--- api/hello.py
+++ api/hello.py
@@ -1,10 +1,13 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+import requests
+
 
 class handler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    return
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        req = requests.get("https://baidu.com/")
+        req.encoding = "utf-8"
+        self.wfile.write(str(req.text).encode())
+        return
