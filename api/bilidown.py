f5fd8b8d6399c6418715759842e739bdd8d6fd16
Fancc666 <fcc565455@163.com>
2024-06-24 18:01:27 +0800

    bilidown允许跨域

--- api/bilidown.py
+++ api/bilidown.py
@@ -11,6 +11,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         urllib3.disable_warnings()
 
